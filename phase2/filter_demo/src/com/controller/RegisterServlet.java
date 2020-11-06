3a2a2c08554c684281de332eb360ed815cd6a06a
vinay.ingalahalli <vinay.ingalahalli@revature.com>
2020-11-06 16:18:57 +0530

    filters demo

---
+++ phase2/filter_demo/src/com/controller/RegisterServlet.java
@@ -0,0 +1,36 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegisterServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//System.out.println("Hello from Servlet");
+	//	System.out.println("Bye Bye from Servlet");
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.print("<h3>Entered PAN "+request.getParameter("pan")+" VALIDATED SUCCESSFULLY</h3>");
+	}
+
+}
